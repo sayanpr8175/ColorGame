0381a68234115f318bd306c6ed3f9e0dc5fed2da
Sayan Pramanik <Sayan.Pramanik8175@gmail.com>
2020-07-22 23:44:47 +0530

    color random matching and comparing implemented

--- colorgame.js
+++ colorgame.js
@@ -1,22 +1,21 @@
-var colors = [
-  "rgb(255, 0, 0)",
-  "rgb(255, 255, 0)",
-  "rgb(0, 255, 0)",
-  "rgb(0, 255, 255)",
-  "rgb(0, 0, 255)",
-  "rgb(255, 0, 255)"
-]
+var colors = generateRandomColors(6);
+
 
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
+
 var messageDisplay = document.querySelector("#message");
 var colordisplay = document.getElementById("colordisplay");
+
 colordisplay.textContent = pickedColor;
 
-for (var i = 0; i<squares.length;i++){
+for (var i = 0; i<squares.length; i++){
+
   squares[i].style.backgroundColor = colors[i];
+
   squares[i].addEventListener("click", function (){
     var clickedcolor = this.style.backgroundColor;
+    console.log(clickedcolor,pickedColor)
     if(clickedcolor === pickedColor){
       messageDisplay.textContent = "Correct !";
       changeColor(clickedcolor);
@@ -40,4 +39,21 @@ function changeColor(color) {
 function pickColor(){
 var random = Math.floor(Math.random() * colors.length);
 return colors[random];
+}
+
+function generateRandomColors(num){
+  // make an array
+  var arr = [];
+  for(var i = 0; i<num; i++){
+    arr.push(randomColor())
+
+  }
+ return arr;
+}
+
+function randomColor(){
+  var r = Math.floor(Math.random()*256);
+  var g = Math.floor(Math.random()*256);
+  var b = Math.floor(Math.random()*256);
+  return "rgb(" + r + ", " + g + ", " + b + ")";
 }
